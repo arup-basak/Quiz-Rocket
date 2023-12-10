f00f696f9eb9f888948357e31586ede17a215f98
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-12-10 23:23:17 +0530

    added catcher

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #[macro_use] extern crate rocket;
 
+use rocket::Request;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .register("/", catchers![internal_error, not_found])
 }
